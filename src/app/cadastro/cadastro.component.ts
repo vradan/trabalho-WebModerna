f57565b7a0089a4fa43215969ed2aecb59972a0e
Lucas Morinigo <lucas.morinigo.94@gmail.com>
2018-10-05 00:44:40 -0300

    inclusão de página de cadastro

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    template: `
+    <div class="container margem">
+    <h1>PÁGINA DE CADASTRO</h1>
+    </div>
+    `
+})
+
+export class CadastroComponent { }

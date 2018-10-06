e7fbd5bdc50e2791c9285e6f7ba8a7e52ad42ddf
Marcos Vinicius <viniciushkd@gmail.com>
2018-10-06 15:13:40 -0300

    correção

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,4 @@
 import { Component } from "@angular/core";
-import { IFuncionario } from './../interfaces/interface.funcionario';
-import { FuncionariosService } from './../services/funcionarios.service';
 
 @Component({
     moduleId: module.id,
